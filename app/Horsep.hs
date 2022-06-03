2e6f52f3f9424f51b8abe4e894e2596762e0848b
Khanh Hua <khanh.hua@doctari.de>
2022-06-03 15:06:23 +0200

    Compose tiny parsers into token parse with Functor, Applicative and Alternativ

--- app/Horsep.hs
+++ app/Horsep.hs
@@ -1,20 +1,71 @@
+{-# LANGUAGE RankNTypes #-}
 module Horsep where
+import Data.Maybe
+import Control.Applicative
+
 
 data Morse
-    = MorseDot
-    | MorseDash
-    | MorseChar Char
-    | MorseInt Int
+  = MorseDot
+  | MorseDash
+  | MorseSpace
+  | MorseChar Char
+  | MorseInt Int
   deriving (Show, Eq)
 
-newtype Parse a = Parse {
-    run :: String -> (a, String)
-}
+newtype Parser a =
+  Parse { run :: String -> (Maybe a, String) }
 
-type MorseParser = Parse Morse
+instance Functor Parser where
+  -- Destructure the internal of Parser
+  fmap f (Parse p) = Parse $ \s ->
+    let
+      (morse, str) = p s
+    in case morse of
+      Nothing -> (Nothing, str)
+      Just morseM -> (Just $ f morseM, str)
+
+instance Applicative Parser where
+  pure a = Parse $ const (Just a, "")
+  (Parse p1) <*> (Parse p2) = Parse $ \s ->
+    let
+      (f, xs1)   = p1 s
+      (m, xs2)  = p2 xs1
+    in (f <*> m, xs2)
+
+instance Alternative Parser where
+  empty = Parse undefined
+  (Parse p1) <|> (Parse p2) = Parse $ \(x:xs) ->
+    let
+      (m1, xs1) = p1 (x:xs)
+      (m2, xs2) = p2 (x:xs)
+    in (m1 <|> m2, xs)
+
+type MorseParser = Parser Morse
+
 
 decodeDot :: MorseParser
-decodeDot = Parse (\('.':xs) -> (MorseDot, xs))
+decodeDot = Parse f
+  where
+    f [] = (Nothing, "")
+    f ('.':xs) = (Just MorseDot, xs)
+    f (_:xs) = (Nothing, xs)
 
 decodeDash :: MorseParser
-decodeDash = Parse (\('-':xs) -> (MorseDash, xs))
+decodeDash = Parse f
+  where
+    f [] = (Nothing, "")
+    f ('-':xs) = (Just MorseDash, xs)
+    f (_:xs) = (Nothing, xs)
+
+decodeSpace :: MorseParser
+decodeSpace = Parse f
+  where
+    f [] = (Nothing, "")
+    f (' ':xs) = (Just MorseSpace, xs)
+    f (_:xs) = (Nothing, xs)
+
+decodeToken :: MorseParser
+decodeToken = decodeDot <|> decodeDash <|> decodeSpace
+
+decodeLetter :: MorseParser
+decodeLetter = undefined
--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,18 @@
 import Horsep
-import Test.Hspec
 import Test.HUnit
+import Test.Hspec
 
 main :: IO ()
-main = hspec $ do
+main =
+  hspec $ do
     describe "Parse elementary components" $ do
-        it "Parse single dot" $ do
-            run decodeDot "." @?= (MorseDot, "")
-        it "Parse single dash" $ do
-            run decodeDash "-" @?= (MorseDash, "")
+      it "Parse single dot" $ do run decodeDot "." @?= (Just MorseDot, "")
+      it "Parse single dash" $ do run decodeDash "-" @?= (Just MorseDash, "")
+      it "Parse space" $ do run decodeSpace " " @?= (Just MorseSpace, "")
+    describe "Parse token" $ do
+      it "Parse token '.'" $ do run decodeToken ". " @?= (Just MorseDot, " ")
+      it "Parse token '-'" $ do run decodeToken "- " @?= (Just MorseDash, " ")
+      it "Parse token ' '" $ do run decodeToken "  " @?= (Just MorseSpace, " ")
+    -- describe "Parse characters" $ do
+    --   it "Parse ." $ do
+    --       run decodeLetter "." @?= (Just $ MorseChar 'i', "")
